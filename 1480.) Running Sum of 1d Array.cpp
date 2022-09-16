c8393c71270fadcbd87eafb2011c1f1002d3e55c
Nirmit Pandya <nirmitrptp@gmail.com>
2022-09-16 20:45:55 +0530

    Array

---
+++ 1480.) Running Sum of 1d Array.cpp
@@ -0,0 +1,26 @@
+// Approach-1
+
+class Solution {
+public:
+    vector<int> runningSum(vector<int>& a) {
+        for(int i=1;i<a.size();i++)
+            a[i]+=a[i-1];
+        return a;
+    }
+};
+
+// Approach-2
+
+class Solution {
+public:
+    vector<int> runningSum(vector<int>& a) {
+        vector<int> v;
+        int sum=0;
+        for(int i=0;i<a.size();i++)
+        {
+            sum+=a[i];
+            v.push_back(sum);
+        }
+        return v;
+    }
+};
