01461f57cc70054e93aa9ca2adc3b1958627a34a
Nirmit Pandya <nirmitrptp@gmail.com>
2022-09-23 22:49:38 +0530

    Linked List

---
+++ 141.) Linked List Cycle.cpp
@@ -0,0 +1,25 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        ListNode *p=head;
+        ListNode *q=head;
+        while(p && p->next)
+        {
+            p=p->next;
+            q=q->next;
+            if(p)
+                p=p->next;
+            if(p==q)
+                return true;
+        }
+        return false;
+    }
+};
