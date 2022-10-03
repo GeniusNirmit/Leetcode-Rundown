a471e6064d452324c1fbf62c861314b5e386744e
Nirmit Pandya <nirmitrptp@gmail.com>
2022-10-03 11:48:45 +0530

    Array

--- 217.) Contains Duplicate.cpp
+++ 217.) Contains Duplicate.cpp
@@ -11,3 +11,18 @@ public:
         return true;
     }
 };
+
+// Approach-2
+
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& a) {
+        sort(a.begin(),a.end());
+        for(int i=0;i<a.size()-1;i++)
+        {
+            if(a[i]==a[i+1])
+                return true;
+        }
+        return false;
+    }
+};
