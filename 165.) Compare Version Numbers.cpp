4551274bd91fa44b4b55367cec087f1b7e1c0a13
Nirmit Pandya <nirmitrptp@gmail.com>
2022-10-06 22:31:44 +0530

    String

--- 165.) Compare Version Numbers.cpp
+++ 165.) Compare Version Numbers.cpp
@@ -1,3 +1,5 @@
+// Approach-1
+
 class Solution {
 public:
     int compareVersion(string version1, string version2) {
@@ -5,7 +7,6 @@ public:
         int l2=version2.length();
         int i=0,j=0;
         int num1,num2;
-        int counter=0;
         while(i<l1 || j<l2)
         {
             num1=0;
@@ -25,6 +26,49 @@ public:
                 return 1;
             else if(num1<num2)
                 return -1;
+            i++;
+            j++;
+        }
+        return 0;
+    }
+};
+
+// Approach-2
+
+class Solution {
+public:
+    int compareVersion(string version1, string version2) {
+        string s1="",s2="";
+        int i=0,j=0;
+        while(i<version1.size() || j<version2.size()) 
+        { 
+            while (i<version1.size() && version1[i]=='0') 
+                i++;
+            while (i<version1.size() && version1[i]!='.') 
+            {
+                s1+=version1[i];
+                i++;
+            }
+            while(j<version2.size() && version2[j]=='0')
+                j++;
+            while(j<version2.size() && version2[j]!='.') 
+            {
+                s2+=version2[j];
+                j++;
+            }
+            if(s1.size()>s2.size())
+                return 1;
+            else if(s1.size()<s2.size())
+                return -1;
+            for(int k=0;k<s1.size();k++) 
+            {
+                if(s1[k]>s2[k]) 
+                   return 1;
+                else if(s1[k]<s2[k])
+                   return -1;
+            }
+            s1="";
+                s2="";
             i++;
             j++;
         }
