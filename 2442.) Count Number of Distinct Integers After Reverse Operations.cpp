db58133840a69a492f072f35da5dad86ef96783c
Nirmit Pandya <nirmitrptp@gmail.com>
2022-10-26 23:59:29 +0530

    Array

---
+++ 2442.) Count Number of Distinct Integers After Reverse Operations.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int countDistinctIntegers(vector<int>& a) {
+        set<int> s(a.begin(),a.end());
+        for(int i=0;i<a.size();i++)
+        {
+            int temp=a[i];
+            string number;
+            while(temp>0)
+            {
+                char x = '0'+(temp%10);
+                number += x;
+                temp/=10;
+            }
+            int num = stoi(number);
+            s.insert(num);
+        }
+        return s.size();
+    }
+};
