e7063f944b00301014a08aea99fb96388c91458e
Nirmit Pandya <nirmitrptp@gmail.com>
2022-10-30 16:55:53 +0530

    Matrix

--- 74.) Search a 2D Matrix.cpp
+++ 74.) Search a 2D Matrix.cpp
@@ -26,3 +26,22 @@ public:
         return false;
     }
 };
+
+// Approach-2
+
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int i = 0, j = matrix[0].size()-1;
+        while(i < matrix.size() && j > -1) 
+        {
+            if (matrix[i][j] == target) 
+                return true;
+            else if (target > matrix[i][j]) 
+                i++;
+            else 
+                j--;
+        }
+        return false;
+    }
+};
