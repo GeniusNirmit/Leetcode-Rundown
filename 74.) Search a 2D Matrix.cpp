947bc6e063fcbaa70da2c1988a3b3ef874acebff
Nirmit Pandya <nirmitrptp@gmail.com>
2022-10-30 16:54:43 +0530

    Matrix

---
+++ 74.) Search a 2D Matrix.cpp
@@ -0,0 +1,28 @@
+// Approach-1
+
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        for(int i=0;i<matrix.size();i++)
+        {
+            if(target == matrix[i][0] || target == matrix[i][matrix[0].size()-1])
+                return true;
+            else if(target < matrix[i][matrix[0].size()-1]  && target > matrix[i][0])
+            {
+                int l = 0, h = matrix[i].size()-1, mid;
+                while(l<=h)
+                {
+                    mid = (l+h)/2;
+                    if(target == matrix[i][mid])
+                        return true;
+                    else if(target > matrix[i][mid])
+                        l = mid + 1;
+                    else 
+                        h = mid - 1;
+                }
+                return false;
+            }   
+        }
+        return false;
+    }
+};
