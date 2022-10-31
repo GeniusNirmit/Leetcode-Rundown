30ec0db5e0da49b1d0d24a75edab0191ac0737a5
Nirmit Pandya <nirmitrptp@gmail.com>
2022-11-01 01:14:33 +0530

    Array

--- 540.) Single Element in a Sorted Array.cpp
+++ 540.) Single Element in a Sorted Array.cpp
@@ -1,3 +1,5 @@
+// Approach-1
+
 class Solution {
 public:
     int singleNonDuplicate(vector<int>& a) {
@@ -8,6 +10,32 @@ public:
         {
             if(i.second==1)
                 return i.first;
+        }
+        return -1;
+    }
+};
+
+// Approach-2
+
+class Solution {
+public:
+    int singleNonDuplicate(vector<int>& a) {
+        if(a.size()==1)
+            return a[0];
+        if(a[0]!=a[1])
+            return a[0];
+        if(a[a.size()-1]!=a[a.size()-2])
+            return a[a.size()-1];
+        int l=0,h=a.size()-1,mid;
+        while(l<=h)
+        {
+            mid = (l+h)/2;
+            if(a[mid] != a[mid+1] && a[mid] != a[mid-1])
+                return a[mid];
+            else if((a[mid] == a[mid+1] && mid%2==0) || (a[mid] == a[mid-1] && mid%2!=0))
+                l = mid + 1;
+            else 
+                h = mid - 1;
         }
         return -1;
     }
