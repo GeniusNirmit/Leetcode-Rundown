4d15431908b236eac2348fc6279861a23c08c1c1
Nirmit Pandya <nirmitrptp@gmail.com>
2022-11-20 00:24:45 +0530

    Matrix

--- 189.) Rotate Array.cpp
+++ 189.) Rotate Array.cpp
@@ -9,3 +9,20 @@ public:
         reverse(a.begin(),a.end());
     }
 };
+
+// Approach-2
+
+    class Solution 
+    {
+    public:
+        void rotate(vector<int> a, int k) 
+        {
+            if(a.size()==0 || k<=0)
+                return;
+            vector<int> v;
+            for(int i=0;i<a.size();i++)
+                v.push_back(a[i]);
+            for(int i=0;i<a.size();i++)
+                a[(i+k)%a.size()] = v[i];
+        }
+    };
