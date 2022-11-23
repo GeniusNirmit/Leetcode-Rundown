2057582d9b5840d72b769b27dd899268d3fbf785
Nirmit Pandya <nirmitrptp@gmail.com>
2022-11-24 00:47:14 +0530

    String

--- 1894.) Find the Student that Will Replace the Chalk.cpp
+++ 1894.) Find the Student that Will Replace the Chalk.cpp
@@ -19,3 +19,31 @@ public:
         return 0;
     }
 };
+
+// Approach-2
+
+class Solution {
+public:
+    int chalkReplacer(vector<int>& chalk, int k) {
+        long long sum = 0;
+        vector<long long> v;
+        for(int i=0;i<chalk.size();i++)
+        {
+            sum += chalk[i];
+            v.push_back(sum);
+        }
+        k %= sum;
+        int l = 0, h = v.size()-1, mid;
+        while(l<h)
+        {
+            mid = (l+h)/2;
+            if(k==v[mid])
+                return mid+1;
+            else if(k<v[mid])
+                h = mid;
+            else
+                l = mid + 1;
+        }
+        return l;
+    }
+};
