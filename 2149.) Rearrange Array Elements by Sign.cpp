96db8c38f0315f7985371d71e06aca7fc02319c0
Nirmit Pandya <nirmitrptp@gmail.com>
2022-11-24 00:39:40 +0530

    Array

--- 2149.) Rearrange Array Elements by Sign.cpp
+++ 2149.) Rearrange Array Elements by Sign.cpp
@@ -21,3 +21,27 @@ public:
         return v;
 	}
 };
+
+// Approach-2
+
+class Solution {
+public:
+    vector<int> rearrangeArray(vector<int>& a) {
+        vector<int> positive;
+        vector<int> negative;
+        for(int i=0;i<a.size();i++)
+        {
+            if(a[i]>=0)
+                positive.push_back(a[i]);
+            else
+                negative.push_back(a[i]);
+        }
+        a.clear();
+        for(int i=0;i<positive.size();i++)
+        {
+            a.push_back(positive[i]);
+            a.push_back(negative[i]);
+        }
+        return a;
+    }
+};
