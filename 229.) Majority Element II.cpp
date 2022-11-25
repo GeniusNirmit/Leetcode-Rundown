7fe3c8f7f7519c1ecbb5b03ff7c9797d3f4f88b0
Nirmit Pandya <nirmitrptp@gmail.com>
2022-11-26 00:10:36 +0530

    Array

--- 229.) Majority Element II.cpp
+++ 229.) Majority Element II.cpp
@@ -16,3 +16,48 @@ public:
         return v;
     }
 };
+
+// Approach-2
+
+class Solution {
+public:
+    vector<int> majorityElement(vector<int>& a) {
+        vector<int> v;
+        int n1,n2,counter1=0,counter2=0;
+        for(int i=0;i<a.size();i++)
+        {
+            if(a[i]==n1)
+                counter1++;
+            else if(a[i]==n2)
+                counter2++;
+            else if(counter1==0)
+            {
+                n1=a[i];
+                counter1++;
+            }
+            else if(counter2==0)
+            {
+                n2=a[i];
+                counter2++;
+            }
+            else
+            {
+                counter1--;
+                counter2--;
+            }
+        }
+        counter1=0,counter2=0;
+        for(int i=0;i<a.size();i++)
+        {
+            if(a[i]==n1)
+                counter1++;
+            if(a[i]==n2)
+                counter2++;
+        }
+        if(counter1>a.size()/3)
+            v.push_back(n1);
+        if(counter2>a.size()/3)
+            v.push_back(n2);
+        return v;
+    }
+};
