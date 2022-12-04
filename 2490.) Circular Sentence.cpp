98e0005256099be0bd330aee50fb26b1f5277c62
Nirmit Pandya <nirmitrptp@gmail.com>
2022-12-04 17:58:05 +0530

    String

---
+++ 2490.) Circular Sentence.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool isCircularSentence(string sentence) {
+        if(sentence[0] != sentence[sentence.size()-1])
+            return false;
+        int i = 1;
+        while(i != sentence.length()-1)
+        {
+            if(sentence[i] == ' ')
+            {
+                if(sentence[i-1] != sentence[i+1])
+                    return false;
+            }
+            i++;
+        }
+        return true;
+    }
+};
