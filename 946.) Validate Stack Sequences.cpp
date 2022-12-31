1a69d75e0ad12cdfcbbbc1a89ad319ac6f1a03f9
Nirmit Pandya <nirmitrptp@gmail.com>
2022-12-31 10:58:52 +0530

    Stack

--- 946.) Validate Stack Sequences.cpp
+++ 946.) Validate Stack Sequences.cpp
@@ -1,3 +1,5 @@
+// Approach-1
+
 class Solution {
 public:
     bool validateStackSequences(vector<int>& pushed, vector<int>& popped) {
@@ -13,6 +15,29 @@ public:
             }
         }
         if(st.empty())
+            return true;
+        return false;
+    }
+};
+
+// Approach-2
+
+class Solution {
+public:
+    bool validateStackSequences(vector<int>& pushed, vector<int>& popped) {
+        int p = 0;
+        int q = 0;
+        for(int i=0;i<pushed.size();i++)
+        {
+            pushed[p] = pushed[i];
+            p++;
+            while(p>0 && pushed[p-1]==popped[q])
+            {
+                p--;
+                q++;
+            }
+        }
+        if(p==0)
             return true;
         return false;
     }
