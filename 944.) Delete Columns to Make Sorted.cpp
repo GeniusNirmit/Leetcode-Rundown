3f2b8167ff8912acfe5a014d154a1d274e0fb630
Nirmit Pandya <nirmitrptp@gmail.com>
2023-01-03 15:14:42 +0530

    Matrix

---
+++ 944.) Delete Columns to Make Sorted.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int minDeletionSize(vector<string>& strs) {
+        int counter = 0;
+        for(int i=0;i<strs[0].length();i++)
+        {
+            for(int j=1;j<strs.size();j++)
+            {
+                if(strs[j][i]<strs[j-1][i])
+                {
+                    counter++;
+                    break;
+                }
+            }
+        }
+        return counter;
+    }
+};
