ad45b3d94daed1c61c1792aed51e9b210982b14d
Nirmit Pandya <nirmitrptp@gmail.com>
2023-01-04 00:44:20 +0530

    Array

---
+++ 2126.) Destroying Asteroids.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool asteroidsDestroyed(long long mass, vector<int>& asteroids) {
+        sort(asteroids.begin(),asteroids.end());
+        for(int i=0;i<asteroids.size();i++)
+        {
+            if(mass < asteroids[i])
+                return false;
+            else
+                mass += asteroids[i];
+        }
+        return true;
+    }
+};
