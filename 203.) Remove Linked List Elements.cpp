ff80eb462bede174a41e6eac9670d4a458306426
Nirmit Pandya <nirmitrptp@gmail.com>
2023-01-05 09:01:03 +0530

    Linked List

---
+++ 203.) Remove Linked List Elements.cpp
@@ -0,0 +1,28 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* removeElements(ListNode* head, int val) {
+        while(head && head->val==val)
+            head=head->next;
+        if(!head)
+            return NULL;
+        ListNode* p=head;
+        while(p->next)
+        {
+            if(p->next->val==val)
+                p->next = p->next->next;
+            else
+                p=p->next;   
+        }
+        return head;
+    }
+};
