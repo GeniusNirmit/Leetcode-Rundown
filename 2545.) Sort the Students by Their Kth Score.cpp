794fc8a8d6da6beeb3f3d18c9d54f6fa5bdbcdb8
Nirmit Pandya <nirmitrptp@gmail.com>
2023-01-23 00:15:19 +0530

    Matrix

---
+++ 2545.) Sort the Students by Their Kth Score.cpp
@@ -0,0 +1,24 @@
+// Approach - 1
+
+class Solution {
+public:
+    vector<vector<int>> sortTheStudents(vector<vector<int>>& score, int k) {
+        if(score[0].size() < k)
+            return score;
+        int n = score.size();
+        vector<pair<int,int>> studentPair;
+        vector<vector<int>> sortedMatrix;
+        for(int i=0;i<n;i++)
+            studentPair.push_back({score[i][k],i});
+        sort(studentPair.begin(),studentPair.end());
+        reverse(studentPair.begin(),studentPair.end());
+        int i = 0;
+        while(n--)
+        {
+            int x = studentPair[i].second;
+            sortedMatrix.push_back(score[x]);
+            i++;
+        }
+        return sortedMatrix;
+    }
+};
