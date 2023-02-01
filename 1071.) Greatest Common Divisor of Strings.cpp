d0d3c5bae17526e64485f77f58ff9cdc9083830e
Nirmit Pandya <nirmitrptp@gmail.com>
2023-02-02 00:13:50 +0530

    String

---
+++ 1071.) Greatest Common Divisor of Strings.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    string gcdOfStrings(string str1, string str2) {
+        if(str1 + str2 != str2 + str1)
+            return "";
+        int gcdStringSize = __gcd(str1.size(),str2.size());
+        return str1.substr(0,gcdStringSize);
+    }
+};
