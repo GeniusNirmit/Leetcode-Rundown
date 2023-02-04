f2dcd0af8dd767748a5b998ae78ab8e885ebb4bc
Nirmit Pandya <nirmitrptp@gmail.com>
2023-02-04 10:32:17 +0530

    String

---
+++ 6.) Zigzag Conversion.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    string convert(string s, int numRows) {
+        if(numRows == 1)
+            return s;
+        int pattern = (numRows-1)*2;
+        int j;
+        string zigzag;
+        for(int i=0;i<numRows;i++)
+        {
+            j = i;
+            while(j < s.length())
+            {
+                zigzag += s[j];
+                if(i != 0 && i != numRows - 1)
+                {
+                    int tempPattern = pattern - 2*i;
+                    int index = j + tempPattern;
+                    if(index < s.length())
+                        zigzag += s[index];
+                }
+                j += pattern;
+            }
+        }
+        return zigzag;
+    }
+};
