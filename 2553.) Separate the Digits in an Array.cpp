45d77573bd8c45eddbeb6bdfc78ff01997d1bb35
Nirmit Pandya <nirmitrptp@gmail.com>
2023-02-05 00:11:06 +0530

    Array

---
+++ 2553.) Separate the Digits in an Array.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<int> separateDigits(vector<int>& a) {
+        vector<int> digits;
+        for(int i=0;i<a.size();i++)
+        {
+            vector<int> temp;
+            while(a[i]>0)
+            {
+                temp.push_back(a[i]%10);
+                a[i] /= 10;
+            }
+            reverse(temp.begin(),temp.end());
+            for(auto j:temp)
+                digits.push_back(j);
+        }
+        return digits;
+    }
+};
