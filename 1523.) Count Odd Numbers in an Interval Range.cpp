213cfdafed9e13b16f7faaff54d001c8640d42aa
Nirmit Pandya <nirmitrptp@gmail.com>
2023-02-14 00:01:44 +0530

    Array

---
+++ 1523.) Count Odd Numbers in an Interval Range.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int countOdds(int low, int high) {
+        if((high-low+1)%2 == 0)
+            return (high-low+1)/2;
+        else
+        {
+            if(low%2 == 0 && high%2 == 0)
+                return (high-low+1)/2;
+            else if(low%2 != 0 || high%2 != 0)
+                return (high-low+1)/2 + 1;
+        }
+        return (high-low+1)/2;
+    }
+};
