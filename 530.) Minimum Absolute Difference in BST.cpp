121fab49ae3cd161e954178a2f04dee10c277138
Nirmit Pandya <nirmitrptp@gmail.com>
2023-02-18 00:05:05 +0530

    Tree

---
+++ 530.) Minimum Absolute Difference in BST.cpp
@@ -0,0 +1,29 @@
+    /**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int prev = -1;
+    int min = INT_MAX;
+    int getMinimumDifference(TreeNode* root) {
+        dfs(root);
+        return min;
+    }
+    void dfs(TreeNode* root) {
+        if(!root)
+            return ;
+        dfs(root->left);
+        if(prev != -1 && abs(root->val - prev) < min)
+            min = abs(root->val - prev);
+        prev = root->val;
+        dfs(root->right);
+    }
+};
