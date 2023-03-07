46d1ab8d8202ad0d46673ddeaafda83425529f56
Nirmit Pandya <nirmitrptp@gmail.com>
2023-03-07 19:37:04 +0530

    Array

---
+++ 2582.) Pass the Pillow.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int passThePillow(int n, int time) {
+        int direction = 1;
+        if((time/(n-1))%2 == 1)
+            direction = -1;
+        
+        if(direction == 1)
+            return 1 + (time % (n-1));
+        return n - (time % (n-1));
+    }
+};
