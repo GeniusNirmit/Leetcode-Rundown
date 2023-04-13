a2688a526004562b479220638f17528ce046164b
Nirmit Pandya <nirmitrptp@gmail.com>
2023-04-13 21:33:10 +0530

    Stack

--- 71.) Simplify Path.cpp
+++ 71.) Simplify Path.cpp
@@ -1,37 +1,38 @@
 class Solution {
 public:
-	string simplifyPath(string path) {
-		stack<string> st;
+    string simplifyPath(string path) {
+        stack<string> st;
         string ans;
 
-		for(int i=0; i<path.size(); i++)
+        for(int i=0; i<path.size(); i++)
         {
-			if(path[i] == '/')
-				continue;
-			string temp;
-			while(i < path.size() && path[i] != '/') 
-    	    {
-				temp += path[i];
-				i++;
-			}
-			if(temp == ".")
-				continue;
-			else if(temp == "..") 
-            {
-				if(!st.empty())
-					st.pop();
-			}
-			else
-				st.push(temp);
-		}
+            if(path[i] == '/')
+                continue;
 
-		while(!st.empty()) 
+            string temp;
+            while(i < path.size() && path[i] != '/')
+            {
+                temp += path[i];
+                i++;
+            }
+            if(temp == ".")
+                continue;
+            else if(temp == "..")
+            {
+                if(!st.empty())
+                    st.pop();
+            }
+            else
+                st.push(temp);
+        }
+
+        while(!st.empty())
         {
-			ans = "/" + st.top() + ans;
-			st.pop();
-		}
-		if(ans.size() == 0)
-			return "/";
-		return ans;
-	}
+            ans = "/" + st.top() + ans;
+            st.pop();
+        }
+        if(ans.size() == 0)
+            return "/";
+        return ans;
+    }
 };
