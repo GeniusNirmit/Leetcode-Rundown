45ab7a2cd52123f6eb38f2ea063f17cde60764c8
Nirmit Pandya <nirmitrptp@gmail.com>
2023-05-01 11:59:54 +0530

    Array

---
+++ 2657.) Find the Prefix Common Array of Two Arrays.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    vector<int> findThePrefixCommonArray(vector<int>& A, vector<int>& B) {
+        unordered_map<int, int> mpA, mpB;
+        vector<int> prefixArray;
+        prefixArray.push_back(0);
+
+        for(int i=0; i<A.size(); i++)
+        {
+            int counter = 0;
+            mpA[A[i]]++;
+            mpB[B[i]]++;
+
+            if(mpA[A[i]] == mpB[A[i]])
+                counter++;
+            
+            if(A[i] != B[i] && mpB[B[i]] == mpA[B[i]])
+                counter++;
+            
+            prefixArray.push_back(prefixArray[i] + counter);
+        } 
+
+        prefixArray.erase(prefixArray.begin());
+        return prefixArray;
+    }
+};
