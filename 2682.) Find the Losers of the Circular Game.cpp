21f174ae14dcfef9ae0b11ec384052657372d22b
Nirmit Pandya <nirmitrptp@gmail.com>
2023-05-14 18:05:52 +0530

    Array

---
+++ 2682.) Find the Losers of the Circular Game.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<int> circularGameLosers(int n, int k) {
+        vector<int> received(n, false);
+        vector<int> losers;
+        received[0] = true;
+        bool flag = true;
+        int i = 1, ind = 0;
+        while(flag)
+        {
+            ind = (ind + i * k) % n;
+            if(received[ind])
+                flag = false;
+            received[ind] = true;
+            i++;
+        }
+        
+        for(int i=0; i<n; i++)
+        {
+            if(!received[i])
+                losers.push_back(i + 1);
+        }
+        return losers;
+    }
+};
