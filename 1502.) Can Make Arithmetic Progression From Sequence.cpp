cc755e8c427804ddde3d4af6a11b04465b398ea3
Nirmit Pandya <nirmitrptp@gmail.com>
2023-06-06 10:23:48 +0530

    Array

---
+++ 1502.) Can Make Arithmetic Progression From Sequence.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool canMakeArithmeticProgression(vector<int>& a) {
+        sort(a.begin(), a.end());
+
+        for(int i=0; i<a.size()-2; i++)
+        {
+            if(a[i+1] - a[i] != a[i+2] - a[i+1])
+                return false;
+        }
+        return true;
+    }
+};
