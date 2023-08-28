a115c4714242c932fbe66ef33ad771e1da44d217
Nirmit Pandya <nirmitrptp@gmail.com>
2023-08-29 00:44:04 +0530

    String

---
+++ 516.) Longest Palindromic Subsequence.cpp
@@ -0,0 +1,27 @@
+class Solution
+{
+public:
+    int longestPalindromeSubseq(string s)
+    {
+        int n = s.length();
+        string reversed = s;
+        reverse(reversed.begin(), reversed.end());
+
+        vector<int> prev(n + 1, 0);
+
+        for (int i = 1; i <= n; i++)
+        {
+            vector<int> current(n + 1, 0);
+            for (int j = 1; j <= n; j++)
+            {
+                if (s[i - 1] == reversed[j - 1])
+                    current[j] = 1 + prev[j - 1];
+                else
+                    current[j] = max(current[j - 1], prev[j]);
+            }
+            prev = current;
+        }
+
+        return prev[n];
+    }
+};
