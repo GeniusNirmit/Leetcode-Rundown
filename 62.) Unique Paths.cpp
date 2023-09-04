3ee4f169bbbd1d7344209c217fd8fb422a3c73b8
Nirmit Pandya <nirmitrptp@gmail.com>
2023-09-04 10:00:08 +0530

    Array

--- 62.) Unique Paths.cpp
+++ 62.) Unique Paths.cpp
@@ -7,17 +7,18 @@ public:
     {
         if (m == 1 && n == 1)
             return 1;
-        vector<vector<int>> dp(m, vector<int>(n, 1));
-        dp[0][0] = 0;
+        vector<int> prev(n, 1);
+        prev[0] = 0;
 
         for (int i = 1; i < m; i++)
         {
+            vector<int> current(n, 1);
             for (int j = 1; j < n; j++)
-            {
-                dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
-            }
+                current[j] = prev[j] + current[j - 1];
+
+            prev = current;
         }
-        return dp[m - 1][n - 1];
+        return prev[n - 1];
     }
 };
 
