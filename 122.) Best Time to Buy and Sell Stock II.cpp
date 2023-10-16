d2dee85dbed5c55f5871977ef2042f41c856edd7
Nirmit Pandya <nirmitrptp@gmail.com>
2023-10-17 00:43:56 +0530

    Array

---
+++ 122.) Best Time to Buy and Sell Stock II.cpp
@@ -0,0 +1,23 @@
+class Solution
+{
+public:
+    int maxProfit(vector<int> &prices)
+    {
+        int n = prices.size();
+        vector<int> prev(2, 0);
+        for (int i = n - 1; i >= 0; i--)
+        {
+            vector<int> current(2, 0);
+            for (int j = 0; j <= 1; j++)
+            {
+                if (j)
+                    current[j] = max(prev[0] - prices[i], prev[1]);
+                else
+                    current[j] = max(prev[1] + prices[i], prev[0]);
+            }
+            prev = current;
+        }
+
+        return prev[1];
+    }
+};
