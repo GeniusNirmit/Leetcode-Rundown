ee642eddc7e6166c1dbc379d803270314f7f7404
Nirmit Pandya <nirmitrptp@gmail.com>
2023-12-12 10:42:40 +0530

    Matrix

---
+++ 867.) Transpose Matrix.cpp
@@ -0,0 +1,17 @@
+class Solution
+{
+public:
+    vector<vector<int>> transpose(vector<vector<int>> &matrix)
+    {
+        int n = matrix.size(), m = matrix[0].size();
+
+        vector<vector<int>> transposeMatrix(m, vector<int>(n));
+
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < m; j++)
+                transposeMatrix[j][i] = matrix[i][j];
+        }
+        return transposeMatrix;
+    }
+};
