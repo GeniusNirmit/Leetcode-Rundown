8ee090e5f9fe5e6d49788712d35e1dde1a8fb684
Nirmit Pandya <nirmitrptp@gmail.com>
2023-12-22 12:45:08 +0530

    Matrix

---
+++ 1637.) Widest Vertical Area Between Two Points Containing No Points.cpp
@@ -0,0 +1,15 @@
+class Solution
+{
+public:
+    int maxWidthOfVerticalArea(vector<vector<int>> &points)
+    {
+        int n = points.size();
+        sort(points.begin(), points.end());
+        int diff = 0;
+
+        for (int i = 0; i < n - 1; i++)
+            diff = max(diff, abs(points[i][0] - points[i + 1][0]));
+
+        return diff;
+    }
+};
