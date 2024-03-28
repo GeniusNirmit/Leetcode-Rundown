8f9ff44b232ef781e3e31ea5a8a08163a8fd4302
Nirmit Pandya <nirmitrptp@gmail.com>
2024-03-28 10:37:55 +0530

    Array

--- 41.) First Missing Positive.cpp
+++ 41.) First Missing Positive.cpp
@@ -1,15 +1,28 @@
-class Solution {
+class Solution
+{
 public:
-    int firstMissingPositive(vector<int>& a) {
-        set<int> s(a.begin(),a.end());
-        int counter=0;
-        for(auto i=s.begin();i!=s.end();++i)
+    int firstMissingPositive(vector<int> &a)
+    {
+        int n = a.size();
+
+        for (int i = 0; i < n; i++)
         {
-            if(!(*i <= 0) && *i != counter+1)
-                return counter+1;
-            else if(!(*i <= 0))
-                counter++;
+            if (a[i] <= 0 || a[i] > n)
+                a[i] = n + 1;
         }
-        return counter+1;
+
+        for (int i = 0; i < n; i++)
+        {
+            if (abs(a[i]) <= n && a[abs(a[i]) - 1] > 0)
+                a[abs(a[i]) - 1] = -a[abs(a[i]) - 1];
+        }
+
+        for (int i = 0; i < n; i++)
+        {
+            if (a[i] > 0)
+                return i + 1;
+        }
+
+        return n + 1;
     }
 };
