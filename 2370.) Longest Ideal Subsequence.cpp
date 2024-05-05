3b17f90f448f6e9fdb2609d4e043d0e647b43d8f
Nirmit Pandya <nirmitrptp@gmail.com>
2024-05-05 08:58:45 +0530

    String

---
+++ 2370.) Longest Ideal Subsequence.cpp
@@ -0,0 +1,30 @@
+// Approach - 1 Recursive Memoization
+
+class Solution
+{
+    int idealString(string s, int k, int ind, int prev, vector<vector<int>> &dp)
+    {
+        int n = s.length();
+
+        if (ind == n)
+            return 0;
+
+        if (dp[ind][prev] != -1)
+            return dp[ind][prev];
+
+        int pick = 0, notPick = 0;
+        if (prev == 0 || int(abs(s[ind] - prev)) <= k)
+            pick = idealString(s, k, ind + 1, s[ind], dp) + 1;
+        notPick = idealString(s, k, ind + 1, prev, dp);
+
+        return dp[ind][prev] = max(pick, notPick);
+    }
+
+public:
+    int longestIdealString(string s, int k)
+    {
+        int n = s.length();
+        vector<vector<int>> dp(n + 1, vector<int>(123, -1));
+        return idealString(s, k, 0, 0, dp);
+    }
+};
