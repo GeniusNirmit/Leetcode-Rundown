743c9e15ca09e4595c648bca5495a47139a9cf8e
Nirmit Pandya <nirmitrptp@gmail.com>
2024-05-10 10:24:30 +0530

    String

--- 3138.) Minimum Length of Anagram Concatenation.cpp
+++ 3138.) Minimum Length of Anagram Concatenation.cpp
@@ -1,18 +1,34 @@
 class Solution
 {
-public:
-    int minAnagramLength(string s)
+    bool isAnagram(string &s, int len)
     {
         int n = s.length();
         vector<int> mp(26, 0);
 
-        for (int i = 0; i < n; i++)
+        for (int i = 0; i < len; i++)
             mp[s[i] - 'a']++;
 
-        int gcd = mp[0];
-        for (int i = 0; i < 26; i++)
-            gcd = __gcd(gcd, mp[i]);
+        for (int i = len; i < n; i += len)
+        {
+            vector<int> currentMp(26, 0);
+            for (int j = i; j < i + len; j++)
+                currentMp[s[j] - 'a']++;
+            if (mp != currentMp)
+                return false;
+        }
+        return true;
+    }
 
-        return n / gcd;
+public:
+    int minAnagramLength(string s)
+    {
+        int n = s.length();
+
+        for (int i = 1; i <= n; i++)
+        {
+            if (n % i == 0 && isAnagram(s, i))
+                return i;
+        }
+        return n;
     }
 };
