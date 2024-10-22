420caad24e9bf17b6b403ac9687dc6ef98eefe2f
Nirmit Pandya <nirmitrptp@gmail.com>
2024-10-22 08:11:50 +0530

    Heap

--- 2583.) Kth Largest Sum in a Binary Tree.cpp
+++ 2583.) Kth Largest Sum in a Binary Tree.cpp
@@ -9,34 +9,41 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-class Solution {
+class Solution
+{
 public:
-    long long kthLargestLevelSum(TreeNode* root, int k) {
-        if(!root)
-            return 0;
-        queue<TreeNode* > q;
-        vector<long long> levelSum;
+    long long kthLargestLevelSum(TreeNode *root, int k)
+    {
+        queue<TreeNode *> q;
+        priority_queue<long long, vector<long long>, greater<long long>> pq;
         q.push(root);
-        while(!q.empty())
+
+        while (!q.empty())
         {
             int size = q.size();
             long long sum = 0;
-            while(size--)
+
+            for (int i = 0; i < size; i++)
             {
-                auto current = q.front();
-                sum += current->val;
+                TreeNode *current = q.front();
                 q.pop();
-                if(current->left)
+
+                sum += current->val;
+
+                if (current->left)
                     q.push(current->left);
-                if(current->right)
+                if (current->right)
                     q.push(current->right);
             }
-            cout<<sum<<endl;
-            levelSum.push_back(sum);
+
+            pq.push(sum);
+
+            if (pq.size() > k)
+                pq.pop();
         }
-        sort(levelSum.begin(),levelSum.end());
-        if(k > levelSum.size())
+
+        if (pq.size() < k)
             return -1;
-        return levelSum[levelSum.size()-k];
+        return pq.top();
     }
 };
