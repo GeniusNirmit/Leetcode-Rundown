e8486f4a929efdf95ba0dbf0718ab3b0dba50e73
Nirmit Pandya <nirmitrptp@gmail.com>
2024-11-06 15:43:16 +0530

    Array

--- 3011.) Find if Array Can Be Sorted.cpp
+++ 3011.) Find if Array Can Be Sorted.cpp
@@ -3,27 +3,30 @@ class Solution
 public:
     bool canSortArray(vector<int> &a)
     {
-        int ind = 0, n = a.size();
-        while (ind < n - 1)
-        {
-            int currSetBits = __builtin_popcount(a[ind]);
-            int nextSetBits = __builtin_popcount(a[ind + 1]);
-            if (a[ind] > a[ind + 1] && currSetBits == nextSetBits)
-            {
-                swap(a[ind], a[ind + 1]);
-                ind = 0;
-            }
-            else if (a[ind] <= a[ind + 1])
-                ind++;
-            else if (a[ind] > a[ind + 1] && currSetBits != nextSetBits)
-                return false;
-        }
+        int n = a.size();
 
         for (int i = 0; i < n - 1; i++)
         {
             if (a[i] > a[i + 1])
-                return false;
+            {
+                if (__builtin_popcount(a[i]) == __builtin_popcount(a[i + 1]))
+                    swap(a[i], a[i + 1]);
+                else
+                    return false;
+            }
         }
+
+        for (int i = n - 1; i >= 1; i--)
+        {
+            if (a[i] < a[i - 1])
+            {
+                if (__builtin_popcount(a[i]) == __builtin_popcount(a[i - 1]))
+                    swap(a[i], a[i - 1]);
+                else
+                    return false;
+            }
+        }
+
         return true;
     }
 };
