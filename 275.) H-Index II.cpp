1a01c58448a31bba36657a8aead00a7ad1194daf
Nirmit Pandya <nirmitrptp@gmail.com>
2026-05-30 23:19:19 +0530

    Array

---
+++ 275.) H-Index II.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int hIndex(vector<int>& citations) {
+        int n = citations.size();
+
+        int low = 0;
+        int high = n - 1;
+
+        while(low <= high) {
+            int mid = low + (high - low) / 2;
+
+            if(citations[mid] <= n - mid - 1) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+
+        return n - low;
+    }
+};
